508a1c8a26afc1f89487dfe4a388b79943a1008e
Niels Falk <niels@nielsfalk.de>
2024-12-27 21:38:40 +0100

    deleteDuplicatesIn

--- composeApp/src/desktopMain/kotlin/de/nielsfalk/desktop/filecleaner/domain/ReadFileInfos.kt
+++ composeApp/src/desktopMain/kotlin/de/nielsfalk/desktop/filecleaner/domain/ReadFileInfos.kt
@@ -1,45 +1,89 @@
 package de.nielsfalk.desktop.filecleaner.domain
 
+import de.nielsfalk.desktop.filecleaner.domain.LoadingState.AdjustScanResult
+import de.nielsfalk.desktop.filecleaner.domain.LoadingState.DeletingFiles
 import de.nielsfalk.desktop.filecleaner.domain.LoadingState.FindDuplicatesInHashedContent
 import de.nielsfalk.desktop.filecleaner.domain.LoadingState.FindDuplicatesInSize
 import de.nielsfalk.desktop.filecleaner.domain.LoadingState.GroupByDirectory
 import de.nielsfalk.desktop.filecleaner.domain.LoadingState.ListFiles
 import de.nielsfalk.desktop.filecleaner.domain.LoadingState.ReadingSize
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.deleteIfExists
 import kotlin.io.path.fileSize
 import kotlin.io.path.isDirectory
 
 object FileReader {
-    fun Set<Path>.scanForDuplicates(
-        loadingStateConsumer: (LoadingState) -> Unit
-    ): ScanResult {
-        loadingStateConsumer(ListFiles)
-        val listResult = listContainingFiles()
-        loadingStateConsumer(ReadingSize)
-        val fileInfos = listResult.toFileInfos()
-        loadingStateConsumer(FindDuplicatesInSize)
-        val duplicatesBySize = fileInfos.duplicatesBySize()
-        loadingStateConsumer(FindDuplicatesInHashedContent)
-        val duplicatesByHashedContentAndSize = duplicatesBySize.duplicatesByHashedContentAndSize()
-        loadingStateConsumer(GroupByDirectory)
-        val countInDirectories = countInDirectories(fileInfos, duplicatesByHashedContentAndSize)
-        return ScanResult(
-            fileInfos = fileInfos,
-            duplicatesByHashedContentAndSize = duplicatesByHashedContentAndSize,
-            countInDirectories = countInDirectories
-        )
-    }
+    suspend fun Set<Path>.scanForDuplicates(
+        loadingStateConsumer: (LoadingState) -> Unit ={}
+    ): ScanResult =
+        withContext(Dispatchers.IO) {
+            loadingStateConsumer(ListFiles)
+            val listResult = listContainingFiles()
+            loadingStateConsumer(ReadingSize)
+            val fileInfos = listResult.toFileInfos()
+            loadingStateConsumer(FindDuplicatesInSize)
+            val duplicatesBySize = fileInfos.duplicatesBySize()
+            loadingStateConsumer(FindDuplicatesInHashedContent)
+            val duplicatesByHashedContentAndSize = duplicatesBySize.duplicatesByHashedContentAndSize()
+            loadingStateConsumer(GroupByDirectory)
+            val countInDirectories = countInDirectories(fileInfos, duplicatesByHashedContentAndSize)
+            ScanResult(
+                fileInfos = fileInfos,
+                duplicatesByHashedContentAndSize = duplicatesByHashedContentAndSize,
+                countInDirectories = countInDirectories
+            )
+        }
 
-  fun deleteDuplicatesIn(
-      directory: Path,
-      scanResult: ScanResult,
-      loadingStateConsumer: (LoadingState) -> Unit,
-      fileDeleter: (Path) -> Unit
-  ) :ScanResult{
+    suspend fun deleteDuplicatesIn(
+        directory: Path,
+        scanResult: ScanResult,
+        loadingStateConsumer: (LoadingState) -> Unit ={},
+        fileDeleter: (Set<Path>) -> Unit = { it.forEach(Path::deleteIfExists) },
+    ): ScanResult =
+        withContext(Dispatchers.IO) {
+            loadingStateConsumer(DeletingFiles)
 
-      return scanResult
-  }
+            val filesToDelete: Set<Path> = scanResult.duplicatesByHashedContentAndSize.values.filter { duplicateFiles ->
+                duplicateFiles.any { it.path.parent == directory }
+            }
+                .flatMap { duplicateFileInfos ->
+                    val duplicateFiles = duplicateFileInfos.map { it.path }
+                    val duplicateInAnotherFolder = duplicateFiles.any { it.parent != directory }
+                    if (duplicateInAnotherFolder) {
+                        duplicateFiles.filter { it.parent == directory }
+                    } else {
+                        val survivingFile = duplicateFiles.maxBy { it.fileName.toString().length }
+                        duplicateFiles.filter { it.parent == directory && it != survivingFile }
+                    }
+                }
+                .toSet()
+
+
+            fileDeleter(filesToDelete)
+
+            loadingStateConsumer(AdjustScanResult)
+            val fileInfos = scanResult.fileInfos.filter { it.path !in filesToDelete }
+            val duplicatesByHashedContentAndSize = scanResult.duplicatesByHashedContentAndSize
+                .map { (duplicationKey, files) ->
+                    duplicationKey to files.filter {
+                        it.path !in filesToDelete
+                    }
+                }
+                .filter { (_, files) -> files.size > 1 }
+                .toMap()
+
+
+            val countInDirectories = countInDirectories(fileInfos, duplicatesByHashedContentAndSize)
+
+            ScanResult(
+                fileInfos = fileInfos,
+                duplicatesByHashedContentAndSize = duplicatesByHashedContentAndSize,
+                countInDirectories = countInDirectories
+            )
+        }
 
     fun Set<Path>.listContainingFiles(): List<Path> =
         flatMap { it.listContainingFiles() }
@@ -67,7 +111,7 @@ object FileReader {
 
         }.toMap()
 
-    fun countInDirectories(
+    internal fun countInDirectories(
         fileInfos: List<FileInfo>,
         groupedDuplicates: Map<*, List<FileInfo>>
     ): List<DirectoryCounts> {
@@ -92,7 +136,9 @@ enum class LoadingState {
     ReadingSize,
     FindDuplicatesInSize,
     FindDuplicatesInHashedContent,
-    GroupByDirectory
+    GroupByDirectory,
+    DeletingFiles,
+    AdjustScanResult
 }
 
 data class FileInfo(
@@ -110,4 +156,8 @@ data class DirectoryCounts(
     val nonDuplicatesCount: Int
 )
 
-data class ScanResult(val fileInfos: List<FileInfo>, val duplicatesByHashedContentAndSize: Map<String, List<FileInfo>>, val countInDirectories: List<DirectoryCounts>)
+data class ScanResult(
+    val fileInfos: List<FileInfo>,
+    val duplicatesByHashedContentAndSize: Map<String, List<FileInfo>>,
+    val countInDirectories: List<DirectoryCounts>
+)
--- composeApp/src/desktopTest/kotlin/de/nielsfalk/desktop/filecleaner/domain/FileReaderTest.kt
+++ composeApp/src/desktopTest/kotlin/de/nielsfalk/desktop/filecleaner/domain/FileReaderTest.kt
@@ -1,14 +1,19 @@
 package de.nielsfalk.desktop.filecleaner.domain
 
 import de.nielsfalk.desktop.filecleaner.domain.FileReader.countInDirectories
+import de.nielsfalk.desktop.filecleaner.domain.FileReader.deleteDuplicatesIn
 import de.nielsfalk.desktop.filecleaner.domain.FileReader.duplicatesByHashedContentAndSize
 import de.nielsfalk.desktop.filecleaner.domain.FileReader.duplicatesBySize
 import de.nielsfalk.desktop.filecleaner.domain.FileReader.listContainingFiles
 import de.nielsfalk.desktop.filecleaner.domain.FileReader.scanForDuplicates
 import de.nielsfalk.desktop.filecleaner.domain.FileReader.toFileInfos
+import de.nielsfalk.desktop.filecleaner.domain.LoadingState.AdjustScanResult
+import de.nielsfalk.desktop.filecleaner.domain.LoadingState.DeletingFiles
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.scopes.FreeSpecContainerScope
 import java.nio.file.Path
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class FileReaderTest : FreeSpec({
     "list files" {
@@ -89,9 +94,7 @@ class FileReaderTest : FreeSpec({
         )
     }
 
-    "directoryCounts"{
-        val basePath = resourceAsPath("testfiles")
-
+    "directoryCounts" {
         val givenFileInfos = setOf(
             resourceAsPath("testfiles"),
             resourceAsPath("othertestfiles/sameContentInOtherDirectory.txt")
@@ -130,10 +133,108 @@ class FileReaderTest : FreeSpec({
 
         givenPaths.scanForDuplicates({ recordedStates += it })
 
-        assertEquals(LoadingState.entries, recordedStates.toList())
+        assertEquals(
+            LoadingState.entries - listOf(DeletingFiles, AdjustScanResult),
+            recordedStates.toList()
+        )
+    }
+
+    "deleteDuplicatesIn" - {
+        data class TestData(
+            val givenPaths: Set<Path>,
+            val deleteDuplicatesIn: Path,
+            val expectedDeletedFiles: Set<Path>,
+            val expectedNewScanResultDirectoryCount: List<DirectoryCounts> = emptyList(),
+            val expectedNewScanResultDuplicatesByHashedContentAndSize: Map<String, List<FileInfo>> = emptyMap(),
+        )
+
+        testOn(
+            TestData(
+                givenPaths = setOf(
+                    resourceAsPath("testfiles"),
+                    resourceAsPath("othertestfiles")
+                ),
+                deleteDuplicatesIn = resourceAsPath("testfiles"),
+                expectedDeletedFiles = setOf(
+                    resourceAsPath("testfiles/sameContent1.txt"),
+                    resourceAsPath("testfiles/sameContent2.txt")
+                )
+            ),
+            TestData(
+                givenPaths = setOf(
+                    resourceAsPath("testfiles"),
+                ),
+                deleteDuplicatesIn = resourceAsPath("testfiles"),
+                expectedDeletedFiles = setOf(
+                    resourceAsPath("testfiles/sameContent1.txt"),
+                    //resourceAsPath("testfiles/sameContent2.txt")
+                )
+            ),
+            TestData(
+                givenPaths = setOf(
+                    resourceAsPath("testfiles"),
+                    resourceAsPath("othertestfiles")
+                ),
+                deleteDuplicatesIn = resourceAsPath("othertestfiles"),
+                expectedDeletedFiles = setOf(
+                    resourceAsPath("othertestfiles/sameContentInOtherDirectory.txt")
+                ),
+                expectedNewScanResultDuplicatesByHashedContentAndSize = mapOf(
+                    "12 pja9fNQgYKTQf6G_vMAQ63eUwrpyHh4-TCAzWhW2bq8=" to listOf(
+                        FileInfo(path = resourceAsPath("testfiles/sameContent2.txt"), size = 12),
+                        FileInfo(path = resourceAsPath("testfiles/sameContent1.txt"), size = 12)
+                    )
+                ),
+                expectedNewScanResultDirectoryCount = listOf(
+                    DirectoryCounts(
+                        directory = resourceAsPath("testfiles"),
+                        duplicatesCount = 2,
+                        nonDuplicatesCount = 2
+                    )
+                )
+            )
+        ) {
+            val scanResult = givenPaths.scanForDuplicates()
+            val recordedFilesToDelete = mutableSetOf<Path>()
+
+            val newScanResult = deleteDuplicatesIn(
+                directory = deleteDuplicatesIn,
+                scanResult = scanResult,
+                fileDeleter = { recordedFilesToDelete += it }
+            )
+
+            assertEquals(
+                expectedDeletedFiles,
+                recordedFilesToDelete
+            )
+            assertTrue(
+                newScanResult.fileInfos.none { it.path in expectedDeletedFiles }
+            )
+            assertEquals(
+                expectedNewScanResultDuplicatesByHashedContentAndSize,
+                newScanResult.duplicatesByHashedContentAndSize
+            )
+            assertEquals(
+                expectedNewScanResultDirectoryCount,
+                newScanResult.countInDirectories
+            )
+        }
+
     }
 
 })
+
+suspend fun <T> FreeSpecContainerScope.testOn(
+    vararg testData: T,
+    function: suspend T.() -> Unit
+) {
+    testData.forEach {
+        "$it" {
+            it.function()
+        }
+
+    }
+}
 
 fun resourceAsPath(name: String): Path =
     Path.of(ClassLoader.getSystemResource(name).toURI())
